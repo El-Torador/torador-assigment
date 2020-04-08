bd04f844fda9d9a746311bc49794c13010d70db8
Jordan Kagmeni <kagmeni77@gmail.com>
2020-04-09 01:52:41 +0200

    refactor code, set severeCasesByRequestedTime prop

--- src/helpers/impact.js
+++ src/helpers/impact.js
@@ -1,21 +1,51 @@
 const impactByDaily = (data) => (
   {
     currentlyInfected: data.reportedCases * 10,
-    infectionsByRequestedTime: Math.floor(((data.reportedCases * 10) * 2) / 3)
+    infectionsByRequestedTime: Math.floor(((data.reportedCases * 10) * (2 ** 10)) / 30),
+    severeCasesByRequestedTime: Math.floor(
+      (
+        (
+          (
+            (
+              data.reportedCases * 10
+            ) * (
+              2 ** 10
+            )
+          ) / 30
+        ) * 15
+      ) / 100
+    )
+    // hospitalBedsByRequestedTime: data.totalHospitalBeds - ((data.totalHospitalBeds * 50) / 100)
   }
 );
 
 const impactByWeekly = (data) => (
   {
     currentlyInfected: impactByDaily(data).currentlyInfected,
-    infectionsByRequestedTime: impactByDaily(data).infectionsByRequestedTime * 7
+    infectionsByRequestedTime: Math.floor((((data.reportedCases * 10) * (2 ** 10)) / 30) * 7),
+    severeCasesByRequestedTime: Math.floor(
+      (
+        (
+          (
+            (
+              (
+                data.reportedCases * 10
+              ) * (
+                2 ** 10
+              )
+            ) / 30
+          ) * 7
+        ) * 15
+      ) / 100
+    )
   }
 );
 
 const impactByMonthly = (data) => (
   {
     currentlyInfected: data.reportedCases * 10,
-    infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** 10)
+    infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** 10),
+    severeCasesByRequestedTime: Math.floor((((data.reportedCases * 10) * (2 ** 10)) * 15) / 100)
   }
 );
 
--- src/helpers/severeImpact.js
+++ src/helpers/severeImpact.js
@@ -1,21 +1,50 @@
 const severeImpactByDaily = (data) => (
   {
     currentlyInfected: data.reportedCases * 50,
-    infectionsByRequestedTime: Math.floor(((data.reportedCases * 50) * 2) / 3)
+    infectionsByRequestedTime: Math.floor(((data.reportedCases * 50) * (2 ** 10)) / 30),
+    severeCasesByRequestedTime: Math.floor(
+      (
+        (
+          (
+            (
+              data.reportedCases * 50
+            ) * (
+              2 ** 10
+            )
+          ) / 30
+        ) * 15
+      ) / 100
+    )
   }
 );
 
 const severeImpactByWeekly = (data) => (
   {
     currentlyInfected: severeImpactByDaily(data).currentlyInfected,
-    infectionsByRequestedTime: severeImpactByDaily(data).infectionsByRequestedTime * 7
+    infectionsByRequestedTime: Math.floor((((data.reportedCases * 50) * (2 ** 10)) / 30) * 7),
+    severeCasesByRequestedTime: Math.floor(
+      (
+        (
+          (
+            (
+              (
+                data.reportedCases * 50
+              ) * (
+                2 ** 10
+              )
+            ) / 30
+          ) * 7
+        ) * 15
+      ) / 100
+    )
   }
 );
 
 const severeImpactByMonthly = (data) => (
   {
     currentlyInfected: data.reportedCases * 50,
-    infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** 10)
+    infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** 10),
+    severeCasesByRequestedTime: Math.floor((((data.reportedCases * 50) * (2 ** 10)) * 15) / 100)
   }
 );
 
