25b49375c87dabc712553950f5aed59717f0a012
Jordan Kagmeni <kagmeni77@gmail.com>
2020-04-08 02:36:32 +0200

    estimations by period

--- src/helpers/impact.js
+++ src/helpers/impact.js
@@ -1,8 +1,22 @@
-const impact = (data) => (
+const impactByDaily = (data) => (
+  {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: Math.floor(((data.reportedCases * 10) * 2) / 3)
+  }
+);
+
+const impactByWeekly = (data) => (
+  {
+    currentlyInfected: impactByDaily(data).currentlyInfected,
+    infectionsByRequestedTime: impactByDaily(data).infectionsByRequestedTime * 7
+  }
+);
+
+const impactByMonthly = (data) => (
   {
     currentlyInfected: data.reportedCases * 10,
     infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** 10)
   }
 );
 
-export default impact;
+export default { impactByDaily, impactByWeekly, impactByMonthly };
--- src/helpers/severeImpact.js
+++ src/helpers/severeImpact.js
@@ -1,8 +1,22 @@
-const severeImpact = (data) => (
+const severeImpactByDaily = (data) => (
+  {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: Math.floor(((data.reportedCases * 50) * 2) / 3)
+  }
+);
+
+const severeImpactByWeekly = (data) => (
+  {
+    currentlyInfected: severeImpactByDaily(data).currentlyInfected,
+    infectionsByRequestedTime: severeImpactByDaily(data).infectionsByRequestedTime * 7
+  }
+);
+
+const severeImpactByMonthly = (data) => (
   {
     currentlyInfected: data.reportedCases * 50,
     infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** 10)
   }
 );
 
-export default severeImpact;
+export default { severeImpactByDaily, severeImpactByWeekly, severeImpactByMonthly };
